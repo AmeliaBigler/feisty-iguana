9980586d0cf3acbee9907fbf638c5b37b4525da7
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2022-12-16 11:32:49 -0600

    add prompts and password choice variables.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
 
+// console.log(Array.from({length: 120}, (v, k) => k+8))
+
+var lengthChoice = Array.from({length: 121}, (v, k) => k+8);
+var caseChoice = ["lowercase","uppercase","both"];
+var specialChoice = ["numeric","special characters","both"];
+
+var generatePassword = function() {
+
+let passwordLength = prompt("Choose a character length for your password. Input a whole number between 8-128:");
+
+if (passwordLength<8 || passwordLength>128) {
+  alert("Your selection is invalid. Please try again.")
+}
+
+let passwordCase = prompt("Choose which letter case types to include in your password. Input upper lower or both:");
+
+if (passwordCase!=="upper" || passwordCase!=="lower" || passwordCase!=="both") {
+  alert("Your selection is invalid. Please try again.")
+}
+
+let passwordSpecial = prompt("Choose to include numbers and/or special characters in your password. Input numbers special or both:")
+
+if (passwordSpecial!=="number" || passwordSpecial!=="special" || passwordSpecial!=="both") {
+  alert("Your selection is invalid. Please try again.")
+}
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
