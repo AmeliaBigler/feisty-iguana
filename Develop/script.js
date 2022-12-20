ac2bfd3addc0e2ef0525b3d849650dd50dcd01a0
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2022-12-20 10:08:02 -0600

    add loop and variables. add comments and restructure/reorganize.

--- Develop/script.js
+++ Develop/script.js
@@ -1,87 +1,104 @@
-// Assignment code here
-
-// console.log(Array.from({length: 121}, (v, k) => k+8))
-
 // variables for prompts
 var lengthChoice = Array.from({length: 121}, (v, k) => k+8);
 var caseChoice = ["lower","upper","both"];
 var specialChoice = ["number","special","both"];
 
-// variables for password array
-var letters = ("abcdefghijklmnopqrstuvwxyz");
-var randomLetter = letters[Math.floor(Math.random()*letters.length)];
-
-var special = ("!@#$%&*");
-var randomSpecial = special[Math.floor(Math.random()*special.length)];
-
-var num = ("0123456789");
-var randomNum = num[Math.floor(Math.random()*num.length)];
-
-// generate password function. runs after generate button is pushed.
+// generate password function. runs after generate button is clicked.
+// after this function completes, return value is used in writePassword function.
 var generatePassword = function() {
 
-let passwordLength = Number(prompt("Choose a character length for your password. Input a whole number between 8-128:"));
+  // Three prompts and alerts.
+  let passwordLength = Number(prompt("Choose a character length for your password. Input a whole number between 8-128:"));
+  if (lengthChoice.includes(passwordLength)===false) 
+    {
+    alert("Your selection is invalid. Please try again.")
+  }; 
 
-if (lengthChoice.includes(passwordLength)===false) 
+  let passwordCase = prompt("Choose which letter case types to include in your password. Input upper lower or both:");
+  if (caseChoice.includes(passwordCase)===false) 
+    {
+    alert("Your selection is invalid. Please try again.")
+  }; 
+
+  let passwordSpecial = prompt("Choose to include numbers and/or special characters in your password. Input number special or both:")
+  if (specialChoice.includes(passwordSpecial)===false)
   {
-  alert("Your selection is invalid. Please try again.")
-}; 
+    alert("Your selection is invalid. Please try again.")
+  } 
 
-let passwordCase = prompt("Choose which letter case types to include in your password. Input upper lower or both:");
+  // variables and functions for password array
+  var letters = ("abcdefghijklmnopqrstuvwxyz");
+  function getRandomLetter () {
+    return letters[Math.floor(Math.random()*letters.length)];
+  }
 
-if (caseChoice.includes(passwordCase)===false) 
-  {
-  alert("Your selection is invalid. Please try again.")
-}; 
+  var special = ("!@#$%&*");
+  function getRandomSpecial () { 
+    return special[Math.floor(Math.random()*special.length)];
+  }
 
-let passwordSpecial = prompt("Choose to include numbers and/or special characters in your password. Input number special or both:")
+  var num = ("0123456789");
+  function getRandomNum () {
+    return num[Math.floor(Math.random()*num.length)];
+  } 
 
-if (specialChoice.includes(passwordSpecial)===false)
- {
-  alert("Your selection is invalid. Please try again.")
-} 
+  // define variable as empty array - to be filled via loop.
+  var userPassword = []
+  
+  // variables for first 4 if's in loop
+  var needsNumber = passwordSpecial === 'number' || passwordSpecial === 'both';
+  var needsSpecial = passwordSpecial === 'special' || passwordSpecial === 'both';
+  var needsUpper = passwordCase === 'upper' || passwordCase === 'both';
+  var needsLower = passwordCase === 'lower' || passwordCase === 'both';
 
-// pseudo-code for creating password string... WIP
-// iterate until length is reached.
-// for true randomization, password array should be scrambled as the last step of the function.
-// var password = String {
-//   length: passwordLength,
-//   case: function(){
-//     if (passwordCase==="lower"){
-//       password[0,1,2,3] = randomLetter; //i++
-//     } else if (passwordCase==="upper"){
-//       password[0,1,2,3] = randomLetter.toUpperCase;
-//     }
-//     else {
-//       password[0,1] = randomLetter;
-//       password[2,3] = randomLetter.toUpperCase;
-//     }
-//   },
+  // TO DO: combo variables/functions for final else in loop will go here.
 
-//   special: function(){
-//     if(passwordSpecial==="special"){
-//       password[4,5,6,7] = randomSpecial;
-//     } else if (passwordSpecial==="number"){
-//       password[4,5,6,7] = randomNum;
-//     } else {
-//       password[4,5] = randomSpecial;
-//       password[6,7] = randomNum;
-//     }
-//   }
-// }
+  // first 4 if's ensure that we have at least one of each required character.
+  // last else fills up the rest of the length with random characters from unique array based on prompt combo.
+  for (let i = 0; i<= passwordLength; i++) {
+
+    let char
+    if (needsNumber) {
+      char = getRandomNum()
+      needsNumber = false
+    } else if (needsSpecial) {
+      char = getRandomSpecial()
+      needsSpecial = false
+    } else if (needsUpper) {
+      char = getRandomLetter().toUpperCase()
+      needsUpper = false
+    } else if (needsLower) {
+      char = getRandomLetter()
+      needsLower = false
+    } else {
+      // TO DO: create unique combo arrays.
+      // if(comboA){}
+      // else if(comboB){}
+      // else if(comboC){}
+      // else{}
+    }
+
+    userPassword.push(char); //pushes chars to the end of the array variable.
+  }
+
+  // TO DO: scramble array here for extra randomization
+  userPassword = userPassword.join(); // TO DO: remove comma separator.
+
+  return userPassword;
+
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() { 
+  var password = generatePassword(); //runs generate password function.
+  var passwordText = document.querySelector("#password"); //defines where password will go by HTML ID.
 
-  passwordText.value = password;
+  passwordText.value = password; //sets defined area to display password that is generated.
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add "click" event listener to generate button. This invokes the function to generate password.
+generateBtn.addEventListener("click", writePassword); 
